7010bd09721fd49e7537e71c9f283361582f4f43
Gli <43455032+gli-27@users.noreply.github.com>
2019-09-12 13:18:50 -0400

    Add files via upload

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,4 +1,4 @@
-import os
+import random
 import torch
 import numpy as np
 from skimage import io
@@ -32,8 +32,8 @@ class FashionDataset(Dataset):
         ])
 
     def __getitem__(self, index):
-        if torch.is_tensor(index):
-            index = index.tolist()
+        # if torch.is_tensor(index):
+        #    index = index.tolist()
 
         row = self.data_df.iloc[index]
         org_img = io.imread(row.tgt_imgPath)
@@ -45,27 +45,26 @@ class FashionDataset(Dataset):
         # row_df = self.data_df.iloc[imgInd]
         # org_imgPath = row_df.tgt_imgPath
         # org_img = io.imread(org_imgPath)
-
         # edge_imgPath = row_df.edge_imgPath
         # edge_img = io.imread(edge_imgPath)
+        # gray_img = 0.2125*org_img[:, :, 0] + 0.7154*org_img[:, :, 1] + 0.0721*org_img[:, :, 2] # 255.0*rgb2gray(orig_img)
+        # gray_img = np.repeat(gray_img[:, :, np.newaxis], 3, axis=2)
 
-        gray_img = 0.2125*org_img[:, :, 0] + 0.7154*org_img[:, :, 1] + 0.0721*org_img[:, :, 2] # 255.0*rgb2gray(orig_img)
-
-        gray_img = np.repeat(gray_img[:, :, np.newaxis], 3, axis=2)
+        # Get Reconstruction img batch
         bbox_x1, bbox_y1 = row.landmark1_x, row.landmark1_y
         bbox_x2, bbox_y2 = row.landmark2_x, row.landmark2_y
         centX, centY = int((bbox_x1 + bbox_x2) / 2.0), int((bbox_y1 + bbox_y2) / 2.0)
         mask = np.zeros((256, 256, 3))
-
         tY2, bY2 = max(centY - 40, 0), min(256, centY + 40)
         tX2, bX2 = max(centX - 40, 0), min(256, centX + 40)
         tY, tX = max(centY - 64, 0), max(0, centX - 64)
         bY, bX = min(tY + 128, 256), min(tX + 128, 256)
         tY, tX = max(bY - 128, 0), max(0, bX - 128)
         mask[tY2:bY2, tX2:bX2, :] = 1.0
-
+        src_img = np.multiply(1.0 - mask, org_img)
         part_edge_img = edge_img[tY:bY, tX:bX, :].copy()
-        src_img = np.multiply(1.0 - mask, org_img)  # np.multiply(mask,blur_img)
+
+        # Get Synthesize img reference
 
         # collar_type = self.type_list.copy()
         # collar_type[row.collar_type] = 1
@@ -74,10 +73,99 @@ class FashionDataset(Dataset):
 
         src_img_tensor = self.org_transform(src_img.astype(np.uint8))
         orig_img_tensor = self.org_transform(np.uint8(org_img))
-        gray_img_tensor = self.transform(np.uint8(gray_img))
+        # gray_img_tensor = self.transform(np.uint8(gray_img))
         part_edge_tensor = self.transform(np.uint8(part_edge_img))
-        return [gray_img_tensor, src_img_tensor, collar_type, orig_img_tensor]
+        return [part_edge_tensor, src_img_tensor, collar_type, orig_img_tensor]
 
     def __len__(self):
         return len(self.data_df)
 
+class InterDataset(Dataset):
+    def __init__(self, opt):
+        self.opt = opt
+        self.data_df = pd.read_csv(opt.data_root + opt.data_path)
+        if self.opt.type_classifier == 'collar':
+            self.type_list = [0] * self.opt.num_collar
+            torch.Tensor(self.type_list)
+        else:
+            self.type_list = [0] * self.opt.num_sleeve
+            torch.Tensor(self.type_list)
+
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((128, 128), interpolation=2),
+            transforms.RandomAffine(degrees=15, scale=(0.85, 1.55), translate=(0.10, 0.0)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+        self.org_transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((128, 128), interpolation=2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+
+    def __getitem__(self, index):
+        # if torch.is_tensor(index):
+        #    index = index.tolist()
+
+        row = self.data_df.iloc[index]
+        org_img = io.imread(row.tgt_imgPath)
+        src_img = io.imread(row.src_imgPath)
+        edge_img = io.imread(row.edge_imgPath)
+        # refer_row = self.data_df.sample(n=1, replace=False, axis=0)
+        idx = random.randint(0, len(self.data_df)-1)
+        refer_row = self.data_df.iloc[idx]
+        refer_edge_img = io.imread(refer_row.edge_imgPath)
+
+
+        # imgInd = np.random.choice(len(self.data_df), 1)[0]
+        # row_df = self.data_df.iloc[imgInd]
+        # org_imgPath = row_df.tgt_imgPath
+        # org_img = io.imread(org_imgPath)
+        # edge_imgPath = row_df.edge_imgPath
+        # edge_img = io.imread(edge_imgPath)
+        # gray_img = 0.2125*org_img[:, :, 0] + 0.7154*org_img[:, :, 1] + 0.0721*org_img[:, :, 2] # 255.0*rgb2gray(orig_img)
+        # gray_img = np.repeat(gray_img[:, :, np.newaxis], 3, axis=2)
+
+        # Get Reconstruction img batch
+        bbox_x1, bbox_y1 = row.landmark1_x, row.landmark1_y
+        bbox_x2, bbox_y2 = row.landmark2_x, row.landmark2_y
+        centX, centY = int((bbox_x1 + bbox_x2) / 2.0), int((bbox_y1 + bbox_y2) / 2.0)
+        mask = np.zeros((256, 256, 3))
+        tY2, bY2 = max(centY - 40, 0), min(256, centY + 40)
+        tX2, bX2 = max(centX - 40, 0), min(256, centX + 40)
+        tY, tX = max(centY - 64, 0), max(0, centX - 64)
+        bY, bX = min(tY + 128, 256), min(tX + 128, 256)
+        tY, tX = max(bY - 128, 0), max(0, bX - 128)
+        mask[tY2:bY2, tX2:bX2, :] = 1.0
+        src_img = np.multiply(1.0 - mask, org_img)
+        part_edge_img = edge_img[tY:bY, tX:bX, :].copy()
+
+        # Get Synthesize img reference
+        bbox_x1, bbox_y1 = refer_row.landmark1_x, refer_row.landmark1_y
+        bbox_x2, bbox_y2 = refer_row.landmark2_x, refer_row.landmark2_y
+        centX, centY = int((bbox_x1 + bbox_x2) / 2.0), int((bbox_y1 + bbox_y2) / 2.0)
+        mask = np.zeros((256, 256, 3))
+        tY2, bY2 = max(centY - 40, 0), min(256, centY + 40)
+        tX2, bX2 = max(centX - 40, 0), min(256, centX + 40)
+        tY, tX = max(centY - 64, 0), max(0, centX - 64)
+        bY, bX = min(tY + 128, 256), min(tX + 128, 256)
+        tY, tX = max(bY - 128, 0), max(0, bX - 128)
+        mask[tY2:bY2, tX2:bX2, :] = 1.0
+        refer_part_edge_img = refer_edge_img[tY:bY, tX:bX, :].copy()
+
+        # collar_type = self.type_list.copy()
+        # collar_type[row.collar_type] = 1
+        # collar_type = torch.Tensor(collar_type)
+        collar_type = refer_row.collar_type
+
+        src_img_tensor = self.org_transform(src_img.astype(np.uint8))
+        orig_img_tensor = self.org_transform(np.uint8(org_img))
+        # gray_img_tensor = self.transform(np.uint8(gray_img))
+        part_edge_tensor = self.transform(np.uint8(part_edge_img))
+        refer_part_edge_tensor = self.transform(np.uint8(refer_part_edge_img))
+        return [part_edge_tensor, refer_part_edge_tensor, src_img_tensor, collar_type, orig_img_tensor]
+
+    def __len__(self):
+        return len(self.data_df)
