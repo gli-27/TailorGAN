cc8b7382fd436e1bb43a083af699de0d1628e1a7
Gli <43455032+gli-27@users.noreply.github.com>
2020-01-30 10:36:40 -0500

    Add files via upload

--- options/options.py
+++ options/options.py
@@ -12,18 +12,18 @@ class CollorOptions():
         self.parser.add_argument('--data_root', type=str, default='/home/cxu-serve/p1/gli27/DATA')
         self.parser.add_argument('--data_path', type=str, default='/Res/mainTrainSet.csv')
         self.parser.add_argument('--pretrain_pkg', type=str, default='/pretrain_pkg/')
-        self.parser.add_argument('--niter', type=int, default=30, help='# of iter at starting learning rate')
-        self.parser.add_argument('--batch_size', type=int, default=4, help='batch size of data loader.')
+        self.parser.add_argument('--niter', type=int, default=120, help='# of iter at starting learning rate')
+        self.parser.add_argument('--batch_size', type=int, default=96, help='batch size of data loader.')
         self.parser.add_argument('--num_workers', type=int, default=4)
-        self.parser.add_argument('--lr', type=float, default=2e-5, help='learning rate.')
+        self.parser.add_argument('--lr', type=float, default=1e-4, help='learning rate.')
         self.parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
         self.parser.add_argument('--isTrain', type=bool, default=False)
-        self.parser.add_argument('--gpuid', type=int, default=2)
+        self.parser.add_argument('--gpuid', type=int, default=0)
         self.parser.add_argument('--n_blocks', type=int, default=6)
         self.parser.add_argument('--use_dropout', type=bool, default=False)
-        self.parser.add_argument('--step', type=str, default='step2', help='step1 is for reconstruction, '
+        self.parser.add_argument('--step', type=str, default='step1', help='step1 is for reconstruction, '
                                                                            'step2 is for synthesize.')
-        self.parser.add_argument('--num_epoch', type=int, default=80)
+        self.parser.add_argument('--num_epoch', type=int, default=40)
 
         self.parser.add_argument('--type_classifier', type=str, default='collar', help='Type of classifier')
         self.parser.add_argument('--num_collar', type=int, default=12, help='Number of collar types')
@@ -37,7 +37,7 @@ class CollorOptions():
                                  help='frequency of showing training results on screen')
         self.parser.add_argument('--save_latest_freq', type=int, default=1000,
                                  help='frequency of saving the latest results')
-        self.parser.add_argument('--save_epoch_freq', type=int, default=10,
+        self.parser.add_argument('--save_epoch_freq', type=int, default=20,
                                  help='frequency of saving checkpoints at the end of epochs')
 
         self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints')
@@ -82,7 +82,7 @@ class SleeveOptions():
         self.parser.add_argument('--use_dropout', type=bool, default=False)
         self.parser.add_argument('--step', type=str, default='step2', help='step1 is for reconstruction, '
                                                                            'step2 is for synthesize.')
-        self.parser.add_argument('--num_epoch', type=int, default=4)
+        self.parser.add_argument('--num_epoch', type=int, default=80)
 
         self.parser.add_argument('--type_classifier', type=str, default='sleeve', help='Type of classifier')
         self.parser.add_argument('--num_collar', type=int, default=12, help='Number of collar types')
@@ -170,3 +170,4 @@ class ClassifierOptions():
         util.mkdirs(expr_dir)
 
         return self.opt
+
