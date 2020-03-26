48c80b45da8ef008bb1a7cb31838148392fb84b6
Gli <43455032+gli-27@users.noreply.github.com>
2020-03-26 11:27:34 -0400

    Update options.py

--- options/options.py
+++ options/options.py
@@ -10,7 +10,7 @@ class CollorOptions():
     def initialize(self):
         self.parser.add_argument('--name', type=str, default='TailorGAN')
         self.parser.add_argument('--data_root', type=str, default='/DATA')
-        self.parser.add_argument('--data_path', type=str, default='/mainTrainSet.csv')
+        self.parser.add_argument('--data_path', type=str, default='/collarTrainSet.csv')
         self.parser.add_argument('--pretrain_pkg', type=str, default='/pretrain_pkg/')
         self.parser.add_argument('--niter', type=int, default=120, help='# of iter at starting learning rate')
         self.parser.add_argument('--batch_size', type=int, default=96, help='batch size of data loader.')
@@ -69,7 +69,7 @@ class SleeveOptions():
     def initialize(self):
         self.parser.add_argument('--name', type=str, default='TailorGAN')
         self.parser.add_argument('--data_root', type=str, default='/DATA')
-        self.parser.add_argument('--data_path', type=str, default='/sleeveTestSet.csv')
+        self.parser.add_argument('--data_path', type=str, default='/sleeveTrainSet.csv')
         self.parser.add_argument('--pretrain_pkg', type=str, default='/pretrain_pkg/')
         self.parser.add_argument('--niter', type=int, default=32, help='# of iter at starting learning rate')
         self.parser.add_argument('--batch_size', type=int, default=72, help='batch size of data loader.')
